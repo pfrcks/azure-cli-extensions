ae49b99cacad90276fcdfb9e11b4367e6bb56db9
Lia Kazakova <liakaz@microsoft.com>
2021-05-25 11:31:21 -0700

    removed accidentally added file

--- testing/test/extensions/public/test.ps1
+++
@@ -1,4 +0,0 @@
-$sslKeyPemFile = Join-Path (Join-Path (Split-Path $PSScriptRoot -Parent) "data") "test_key.pem"
-$sslCertPemFile = Join-Path (Join-Path (Split-Path $PSScriptRoot -Parent) "data") "test_cert.pem"
-
-Write-Output $sslKeyPemFile
