e670203eb8e6084a126d03546254bacbf86af3d3
Jonathan Innis <jonathan.innis.ji@gmail.com>
2021-07-06 12:39:31 -0700

    Add formatters

--- src/k8s-config/azext_k8s_config/commands.py
+++ src/k8s-config/azext_k8s_config/commands.py
@@ -10,6 +10,14 @@ from azext_k8s_config._client_factory import (
     k8s_config_extension_client,
     k8s_config_sourcecontrol_client
 )
+from .format import (
+    fluxconfig_list_table_format,
+    fluxconfig_show_table_format,
+    extension_list_table_format,
+    extension_show_table_format,
+    sourcecontrol_list_table_format,
+    sourcecontrol_show_table_format
+)
 
 
 def load_command_table(self, _):
@@ -30,20 +38,20 @@ def load_command_table(self, _):
 
     with self.command_group('k8s-config flux', k8s_config_fluxconfig_sdk, client_factory=k8s_config_fluxconfig_client, is_preview=True) as g:
         g.custom_command('create', 'flux_config_create')
-        g.command('list', "list")
-        g.custom_command('show', 'flux_config_show')
+        g.custom_command('list', "flux_config_list", table_transformer=fluxconfig_list_table_format)
+        g.custom_command('show', 'flux_config_show', table_transformer=fluxconfig_show_table_format)
         g.custom_command('delete', 'flux_config_delete', confirmation=True)
-        g.custom_command('source create', 'flux_config_create_source', supports_local_cache=True)
-        g.custom_command('kustomization create', 'flux_config_create_kustomization', supports_local_cache=True)
+        # g.custom_command('source create', 'flux_config_create_source', supports_local_cache=True)
+        # g.custom_command('kustomization create', 'flux_config_create_kustomization', supports_local_cache=True)
 
     with self.command_group('k8s-config extension', k8s_config_extension_sdk, client_factory=k8s_config_extension_client, is_preview=True) as g:
         g.custom_command('create', 'extension_create')
-        g.custom_command('list', "extension_list")
-        g.custom_command('show', 'extension_show')
+        g.custom_command('list', "extension_list", table_transformer=extension_list_table_format)
+        g.custom_command('show', 'extension_show', table_transformer=extension_show_table_format)
         g.custom_command('delete', 'extension_delete', confirmation=True)
 
-    # with self.command_group('k8s-config source', k8s_config_sourcecontrol_sdk, client_factory=k8s_config_sourcecontrol_client) as g:
-    #     g.custom_command('create', 'source_create')
-    #     g.custom_command('list', 'list_k8sconfiguration')
-    #     g.custom_show_command('show', 'show_k8sconfiguration')
-    #     g.custom_command('delete', 'delete_k8sconfiguration', confirmation=True)
+    with self.command_group('k8s-config flux-v1', k8s_config_sourcecontrol_sdk, client_factory=k8s_config_sourcecontrol_client) as g:
+        g.custom_command('create', 'sourcecontrol_create')
+        g.custom_command('list', 'sourcecontrol_list', table_transformer=sourcecontrol_list_table_format)
+        g.custom_show_command('show', 'sourcecontrol_show', table_transformer=sourcecontrol_show_table_format)
+        g.custom_command('delete', 'sourcecontrol_delete', confirmation=True)
---
+++ src/k8s-config/azext_k8s_config/format.py
@@ -0,0 +1,58 @@
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
+
+from collections import OrderedDict
+
+
+def sourcecontrol_list_table_format(results):
+    return [__get_sourcecontrolconfig_table_row(result) for result in results]
+
+
+def sourcecontrol_show_table_format(result):
+    return __get_sourcecontrolconfig_table_row(result)
+
+
+def __get_sourcecontrolconfig_table_row(result):
+    return OrderedDict([
+        ('name', result['name']),
+        ('repositoryUrl', result['repositoryUrl']),
+        ('operatorName', result['operatorInstanceName']),
+        ('operatorNamespace', result['operatorNamespace']),
+        ('scope', result['operatorScope']),
+        ('provisioningState', result['provisioningState'])
+    ])
+
+
+def extension_list_table_format(results):
+    return [__get_extension_table_row(result) for result in results]
+
+
+def extension_show_table_format(result):
+    return __get_extension_table_row(result)
+
+
+def __get_extension_table_row(result):
+    return OrderedDict([
+        ('name', result['name']),
+        ('extensionType', result['extensionType']),
+        ('version', result['version']),
+        ('installState', result['installState']),
+        ('lastModifiedTime', result['lastModifiedTime'])
+    ])
+
+
+def fluxconfig_list_table_format(results):
+    return [__get_fluxconfig_table_row(result) for result in results]
+
+
+def fluxconfig_show_table_format(result):
+    return __get_fluxconfig_table_row(result)
+
+
+def __get_fluxconfig_table_row(result):
+    print(result)
+    return OrderedDict([
+        ('name', result['name']),
+    ])
