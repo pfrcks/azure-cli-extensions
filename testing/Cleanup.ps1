330a4e8ad30a8ede063d5de5b96a2de8e78fc57f
Jonathan Innis <jonathan.innis.ji@gmail.com>
2021-07-19 14:13:01 -0700

    Add debug for failed cleanup

--- testing/Cleanup.ps1
+++ testing/Cleanup.ps1
@@ -15,6 +15,7 @@ az connectedk8s delete -g $ENVCONFIG.resourceGroup -n $ENVCONFIG.arcClusterName
 if (!$?)
 {
     kubectl get pods -A
+    kubectl logs -l app.kubernetes.io/component=cluster-metadata-operator -n azure-arc -c cluster-metadata-operator
     Exit 1
 }
 
